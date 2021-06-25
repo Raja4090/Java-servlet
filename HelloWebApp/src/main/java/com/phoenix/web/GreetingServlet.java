67e83760ba96145ad07c52fd1df8cc589bb66de9
Raja Kumar <rajakumarjha612@gmail.com>
2021-06-26 00:04:18 +0530

    Simple Dynamic web Application using Servlet

---
+++ HelloWebApp/src/main/java/com/phoenix/web/GreetingServlet.java
@@ -0,0 +1,66 @@
+package com.phoenix.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+/**
+ * Servlet implementation class GreetingServlet
+ * @author: raja.kumar@stltech.in
+ */
+
+public class GreetingServlet implements Servlet {
+
+    /**
+     * Default constructor. 
+     */
+    public GreetingServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		pw.println("<h1 style=color:green;>This is gonna be alluring.....!</h1>");
+	}
+
+}
